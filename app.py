e019485b53c8c744b7623e5b1dd98085b4dec217
festusmbah04 <festusmbahgitup@gmail.com>
2024-05-25 14:04:16 -0700

    hello

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'randersson014@gmail.com'  # Your Gmail email address
-app.config['MAIL_PASSWORD'] = 'xcogieahmrhvedab'  # Your Gmail password or App Password
+app.config['MAIL_USERNAME'] = 'asy.tradl@gmail.com'  # Your Gmail email address
+app.config['MAIL_PASSWORD'] = 'jtfikjcntciixksc'  # Your Gmail password or App Password
 
 mail = Mail(app)
 
